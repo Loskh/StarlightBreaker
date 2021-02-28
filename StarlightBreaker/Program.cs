9513d02e4cd3e616197c752c40f0e14c4f68f449
PrototypeSeiran <1020612624@qq.com>
2021-02-28 19:21:35 +0800

    [feat] 支持招募反和谐

--- StarlightBreaker/Program.cs
+++ StarlightBreaker/Program.cs
@@ -1,4 +1,4 @@
-﻿using Advanced_Combat_Tracker;
+using Advanced_Combat_Tracker;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,28 +13,23 @@ using Zodiark.MemoryPatch;
 
 namespace StarlightBreaker
 {
-    class MainClass : IActPluginV1  
+    class MainClass : IActPluginV1
     {
         private Label statusLabel;
-        private Label lbl;
         private Process FFXIV;
-        private Patch StarPatch;
+        private Patch ChatLogStarPatch;
+        private Patch pfinderStarPatch;
+        private Patch pfinderDialogStarPatch;
         private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin ffxivPlugin;
         private BackgroundWorker _processSwitcher;
         private ZodiarkProcess Mordion;
 
 
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText) {
-            pluginScreenSpace.Text = "Starlight Breaker";
             statusLabel = pluginStatusText;
             ffxivPlugin = GetFfxivPlugin();
-            pluginScreenSpace.Text = "Starlight Breaker";
-            this.lbl = new Label {
-                Location = new Point(8, 8),
-                AutoSize = true,
-                Text = "Looking for FFXIV..."
-            };
-            pluginScreenSpace.Controls.Add(this.lbl);
+
+            statusLabel.Text = "Looking for FFXIV...";
 
             _processSwitcher = new BackgroundWorker { WorkerSupportsCancellation = true };
             _processSwitcher.DoWork += ProcessSwitcher;
@@ -62,7 +57,7 @@ namespace StarlightBreaker
                     FFXIV = GetFFXIVProcess();
                     if (FFXIV != null)
                         Attach();
-                    else lbl.Text = "Looking for FFXIV...";
+                    else statusLabel.Text = "Looking for FFXIV...";
                 }
 
                 Thread.Sleep(3000);
@@ -70,21 +65,46 @@ namespace StarlightBreaker
         }
 
         private void Detach() {
-            if (Mordion != null && StarPatch != null) {
-                StarPatch.Disable();
-                lbl.Text = "反和谐已关闭";
-                statusLabel.Text = "Disable";
+            if (Mordion != null) {
+                if (ChatLogStarPatch != null) {
+                    ChatLogStarPatch.Disable();
+                }
+                if (pfinderStarPatch != null) {
+                    pfinderStarPatch.Disable();
+                }
+                if (pfinderDialogStarPatch != null) {
+                    pfinderDialogStarPatch.Disable();
+                }
             }
-
+            statusLabel.Text = "反和谐已关闭";
         }
 
         private void Attach() {
-            Mordion = new ZodiarkProcess(FFXIV);
-            var SkipAddress = Mordion.Scanner.ScanText("74 ?? 48 8B D3 E8 ?? ?? ?? ?? 48 8B C3");
-            StarPatch = Mordion.SetPatch(SkipAddress, new byte?[] { 0xEB });
-            StarPatch.Enable();
-            lbl.Text = "反和谐已开启";
-            statusLabel.Text = "Enable";
+            try {
+                if (FFXIV.ProcessName == "ffxiv_dx11.exe") {
+                    Mordion = new ZodiarkProcess(FFXIV);
+                    var ChatLogSkipAddress = Mordion.Scanner.ScanText("74 ?? 48 8B D3 E8 ?? ?? ?? ?? 48 8B C3");
+                    ChatLogStarPatch = Mordion.SetPatch(ChatLogSkipAddress, new byte?[] { 0xEB });
+                    var pfinderSkipAddress = Mordion.Scanner.ScanText("48 8B D6 E8 ?? ?? ?? ?? 80 BF") + 3;
+                    pfinderStarPatch = Mordion.SetPatch(pfinderSkipAddress, new byte?[] { 0x90, 0x90, 0x90, 0x90, 0x90 });
+                    var pfinderDialogSkipAddress = Mordion.Scanner.ScanText("4C 8B C7 E8 ?? ?? ?? ?? 40 38 B3") + 3;
+                    pfinderDialogStarPatch = Mordion.SetPatch(pfinderDialogSkipAddress, new byte?[] { 0x90, 0x90, 0x90, 0x90, 0x90 });
+
+                    ChatLogStarPatch.Enable();
+                    pfinderStarPatch.Enable();
+                    pfinderDialogStarPatch.Enable();
+                    statusLabel.Text = "反和谐已开启";
+                }
+                else {
+                    MessageBox.Show($"2021年了，别用Dx9了", "幹，老兄你的游戏好雞瓣怪啊",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    throw new Exception("不支持FFXIV Dx9");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"反和谐开启失败！\n{ex.Message}");
+                statusLabel.Text = "反和谐开启失败";
+            }
+
         }
 
         private Process GetFFXIVProcess() {
@@ -93,8 +113,7 @@ namespace StarlightBreaker
 
         public void DeInitPlugin() {
             try {
-                if (Mordion != null&&StarPatch!=null)
-                    Detach();
+                Detach();
             }
             catch (Exception) {
                 // ignored
