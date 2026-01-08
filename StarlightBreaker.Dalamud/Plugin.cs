acf99b8ca5672511122b4aa71596053c08bad357
Loskh <1020612624@qq.com>
2026-01-08 12:26:36 +0800

    fix: change sig to resolve conflict

--- StarlightBreaker.Dalamud/Plugin.cs
+++ StarlightBreaker.Dalamud/Plugin.cs
@@ -5,11 +5,14 @@ using Dalamud.Interface.Windowing;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
+using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.System.Memory;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.Text;
 using InteropGenerator.Runtime;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using UTF8String = FFXIVClientStructs.FFXIV.Client.System.String.Utf8String;
@@ -57,14 +60,14 @@ namespace StarlightBreaker
         //private Hook<VulgarCheckDelegate> VulgarCheckHook;
         //private VulgarCheckDelegate VulgarCheck;
 
-        public delegate bool AgentLookingForGroupTextFilterDelegate(AgentLookingForGroup* agent, Utf8String* text);
+        public delegate void AgentLookingForGroupTextFilterDelegate(AgentLookingForGroup* agent, Utf8String* text);
         Hook<AgentLookingForGroupTextFilterDelegate> AgentLookingForGroupTextFilterHook;
 
         public delegate Utf8String* RaptureTextModuleChatLogFilterDelegate(RaptureTextModule* textModule, Utf8String* text, nint unk, uint bytesNum);
         Hook<RaptureTextModuleChatLogFilterDelegate> RaptureTextModuleChatLogFilterHook;
 
-
-        public delegate bool RaptureTextModulePartyFinderFilterDelegate(RaptureTextModule* textModule, Utf8String* text, nint unk, bool unk1);
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        public delegate void RaptureTextModulePartyFinderFilterDelegate(RaptureTextModule* textModule, Utf8String* text, nint unk, bool unk1);
         private RaptureTextModulePartyFinderFilterDelegate RaptureTextModulePartyFinderFilterOrigin;
         private CallHook<RaptureTextModulePartyFinderFilterDelegate> AgentLookingForGroupDetailedWindowTextFilterHook;
 
@@ -91,13 +94,18 @@ namespace StarlightBreaker
             this.RaptureTextModuleChatLogFilterHook = GameInteropProvider.HookFromSignature<RaptureTextModuleChatLogFilterDelegate>("40 53 48 83 EC 20 48 8D 99 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 48 8B 0D", this.RaptureTextModuleChatLogFilterDetour);
             this.RaptureTextModuleChatLogFilterHook.Enable();
 
-            this.RaptureTextModulePartyFinderFilterOrigin = Marshal.GetDelegateForFunctionPointer<RaptureTextModulePartyFinderFilterDelegate>(Scanner.ScanText("E8 ?? ?? ?? ?? 44 38 A3 ?? ?? ?? ?? 74 11"));
-
             var hookAddress = Scanner.ScanAllText("E8 ?? ?? ?? ?? 44 38 A3 ?? ?? ?? ?? 74 ?? 48 8D 15").First();
+            PluginLog.Debug($"AgentLookingForGroupDetailedWindowTextFilterHook:{Util.DescribeAddress(hookAddress)}");
             this.AgentLookingForGroupDetailedWindowTextFilterHook = new CallHook<RaptureTextModulePartyFinderFilterDelegate>(hookAddress, RaptureTextModulePartyFinderFilter);
-            // 暂时关闭
-            // this.AgentLookingForGroupDetailedWindowTextFilterHook?.Enable();
+            this.AgentLookingForGroupDetailedWindowTextFilterHook?.Enable();
 
+
+            //由于"E8 ?? ?? ?? ?? 44 38 A3 ?? ?? ?? ?? 74 11"和"E8 ?? ?? ?? ?? 44 38 A3 ?? ?? ?? ?? 74 ?? 48 8D 15"是同一地址的不同签名ffxiv_dx11.exe+0x5419A3 函数地址为ffxiv_dx11.exe+0x97B820
+            //之前的版本已经被Hook，因此开头不再是E9或E8,因此后面的ScanText会返回ffxiv_dx11.exe+0x5419A3,而不是0x97B820
+            //var raptureTextModulePartyFinderFilterAddress = Scanner.ScanText("E8 ?? ?? ?? ?? 44 38 A3 ?? ?? ?? ?? 74 11");
+            var raptureTextModulePartyFinderFilterAddress = Scanner.ScanText("40 53 56 57 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 8B F9 49 8B F0");
+            PluginLog.Debug($"RaptureTextModulePartyFinderFilter:{Util.DescribeAddress(raptureTextModulePartyFinderFilterAddress)}");
+            this.RaptureTextModulePartyFinderFilterOrigin = Marshal.GetDelegateForFunctionPointer<RaptureTextModulePartyFinderFilterDelegate>(raptureTextModulePartyFinderFilterAddress);
             //TODO:
             //E8 ?? ?? ?? ?? 0F B6 BB ?? ?? ?? ?? 40 84 FF 74 ?? 48 8D 15
             //当出现无法处理招募的时候检查文本并显示导致无法发送的地方
@@ -111,7 +119,7 @@ namespace StarlightBreaker
             }
         }
 
-        private bool RaptureTextModulePartyFinderFilter(RaptureTextModule* textModule, UTF8String* text, nint unk, bool unk1)
+        private void RaptureTextModulePartyFinderFilter(RaptureTextModule* textModule, UTF8String* text, nint unk, bool unk1)
         {
             if (this.Configuration.PartyFinderConfig.Enable)
             {
@@ -122,9 +130,9 @@ namespace StarlightBreaker
                         var original = IMemorySpace.GetDefaultSpace()->Create<Utf8String>();
                         original->Ctor();
                         original->Copy(text);
-                        var ret = RaptureTextModulePartyFinderFilterOrigin(textModule, text, unk, unk1);
+                        RaptureTextModulePartyFinderFilterOrigin(textModule, text, unk, unk1);
                         if (text->EqualTo(original))
-                            return ret;
+                            return;
                         var result = HighlightCensoredParts(original->ToString(), text->ToString(), this.Configuration.FontConfig.Italics, this.Configuration.FontConfig.EnableColor, this.Configuration.FontConfig.Color);
                         var bytes = result.Encode();
                         fixed (byte* pointer = bytes)
@@ -132,15 +140,15 @@ namespace StarlightBreaker
                             text->SetString((CStringPointer)pointer);
                         }
                         original->Dtor(true);
-                        return ret;
+                        return;
                     }
                 }
-                return true;
+                return;
             }
             else
             {
-                var ret = RaptureTextModulePartyFinderFilterOrigin(textModule, text, unk, unk1);
-                return ret;
+                RaptureTextModulePartyFinderFilterOrigin(textModule, text, unk, unk1);
+                return;
             }
         }
 
@@ -174,7 +182,7 @@ namespace StarlightBreaker
 
         }
 
-        private unsafe bool AgentLookingForGroupTextFilterDetour(AgentLookingForGroup* agent, UTF8String* text)
+        private void AgentLookingForGroupTextFilterDetour(AgentLookingForGroup* agent, UTF8String* text)
         {
             if (this.Configuration.PartyFinderConfig.Enable)
             {
@@ -185,9 +193,9 @@ namespace StarlightBreaker
                         var original = IMemorySpace.GetDefaultSpace()->Create<Utf8String>();
                         original->Ctor();
                         original->Copy(text);
-                        var ret = this.AgentLookingForGroupTextFilterHook.Original(agent, text);
+                        this.AgentLookingForGroupTextFilterHook.Original(agent, text);
                         if (text->EqualTo(original))
-                            return ret;
+                            return;
                         var result = HighlightCensoredParts(original->ToString(), text->ToString(), this.Configuration.FontConfig.Italics, this.Configuration.FontConfig.EnableColor, this.Configuration.FontConfig.Color);
                         var bytes = result.Encode();
                         fixed (byte* pointer = bytes)
@@ -197,9 +205,13 @@ namespace StarlightBreaker
                         original->Dtor(true);
                     }
                 }
-                return true;
+                return;
             }
-            return this.AgentLookingForGroupTextFilterHook.Original(agent, text);
+            else
+            {
+                this.AgentLookingForGroupTextFilterHook.Original(agent, text);
+                return;
+            }
         }
 
         private void DrawUI() => WindowSystem.Draw();
